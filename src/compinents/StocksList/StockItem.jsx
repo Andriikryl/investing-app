aeea7e480f0915ce39b8dbb35005d4d09806e3c4
Andriikryl <andreyk557@gmail.com>
2023-04-09 23:48:19 +0300

    feat[price/name/curance]

--- src/compinents/StocksList/StockItem.jsx
+++ src/compinents/StocksList/StockItem.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import stocksApi from '../api/stocksApi'
+import { useQuery } from 'react-query'
+
+const fetchStock = async (symbol) => {
+    const price = await stocksApi['getPrice'](symbol)
+    const profile = await stocksApi['getProfile'](symbol)
+
+    const stock = {...profile.data, price: price.data.c}
+    return stock
+}
 
 export default function StockItem({stock, deleteFromFavorite}) {
+    const {data, isLoading} = useQuery(`getStock/${stock.symbol}`, () => fetchStock(stock.symbol))
+    console.log(data)
   return (
     <li className='stock-group'>
+        {data ? <div>
+        {data.logo ? <img className='logo' src={data.logo} alt={data.name}></img> : <div></div>}
+        <div>
+            <p>{data.name}</p>
+            <p>{data.ticker}</p>
+            <p>{data.price} {data.currency} </p>
+        </div>
+        </div> : null}
         {stock.description}
         <button onClick={()=> deleteFromFavorite(stock.symbol)}>delete</button>
     </li>
--- src/compinents/api/stocksApi.js
+++ src/compinents/api/stocksApi.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 const API_KEY = "cgpe8ehr01qo64fgi3p0cgpe8ehr01qo64fgi3pg";
 const BASE_URL = "https://finnhub.io/api/v1";
+// curl "https://finnhub.io/api/v1/stock/profile2?symbol=AAPL&token=cgpe8ehr01qo64fgi3p0cgpe8ehr01qo64fgi3pg"
+
+// curl "https://finnhub.io/api/v1/quote?symbol=AAPL&token=cgpe8ehr01qo64fgi3p0cgpe8ehr01qo64fgi3pg"
 
 const stocksApi = {
   getStocks: async () => {
@@ -9,6 +12,11 @@ const stocksApi = {
     );
     return response.data;
   },
+  getProfile: (symbol) =>
+    axios.get(`${BASE_URL}/stock/profile2?symbol=${symbol}&token=${API_KEY}`),
+
+  getPrice: (symbol) =>
+    axios.get(`${BASE_URL}/quote?symbol=${symbol}&token=${API_KEY}`),
 };
 
 export default stocksApi;
